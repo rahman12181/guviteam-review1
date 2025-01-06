cea201651299a6c7a4003c22dd32cd9fdb73f1b5
LUTFUR RAHMAN <lutfur1218@gmail.com>
2025-01-06 14:33:31 +0530

    Delete LogOut.java

--- LogOut.java
+++
@@ -1,8 +0,0 @@
-public class LogOut {
-
-    public void logOutUser() {
-        // Add your log out logic here
-        System.out.println("User has been logged out.");
-        // For example, you might want to invalidate a session or clear user data
-    }
-}
